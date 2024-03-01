15df692a08941774c264f122dc3dd6b46cc46fba
thanh-ukpc <thanh.do@ukparkingcontrol.com>
2024-03-01 11:16:19 +0000

    update submodule

--- src/submodules/ecommerce-ops-base-model/base-model/Identifiable.ts
+++
@@ -1,3 +0,0 @@
-export interface Identifiable {
-    Id?: number;
-}
--- src/submodules/ecommerce-ops-base-model/base-model/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Identifiable'
-export * from './BaseModel'
-
-export * from './Paging'
-export * from './Query'
-export * from './CommonType'
